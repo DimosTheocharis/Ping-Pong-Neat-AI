ca1bbd376e304f3ffc77252ed011ae45db344ab8
Dimos Theocharis <dimostheocharis10@gmail.com>
2024-10-04 12:07:59 +0300

    Another test for FeedForwardNetwork.activate method

--- Neat/tests/feedForwardNetwork.test.ts
+++ Neat/tests/feedForwardNetwork.test.ts
@@ -163,6 +163,83 @@ describe("activate function", () => {
 
             // Tests
             expect(output.length).toBe(1);
-            expect(output[0]).toBeCloseTo(0.369, 2);
+            expect(output[0]).toBeCloseTo(0.369, 3);
+        })
+
+
+
+    it("Should validate that the output of the network with 2 input, 3 hidden nodes and 3 output, for given input, \
+        is correct", () => {
+            const myGenome: Genome = new Genome(1, 0, 0);
+
+            // Nodes creation
+            const node1: NodeGene = new NodeGene(1, NodeGeneType.INPUT);
+            const node2: NodeGene = new NodeGene(2, NodeGeneType.INPUT);
+            const node3: NodeGene = new NodeGene(3, NodeGeneType.OUTPUT);
+            const node4: NodeGene = new NodeGene(4, NodeGeneType.OUTPUT);
+            const node5: NodeGene = new NodeGene(5, NodeGeneType.OUTPUT);
+            const node6: NodeGene = new NodeGene(6, NodeGeneType.HIDDEN);
+            const node7: NodeGene = new NodeGene(7, NodeGeneType.HIDDEN);
+            const node8: NodeGene = new NodeGene(8, NodeGeneType.HIDDEN);
+
+            // Set bias for all nodes except input nodes
+            node3._bias = 0.36;
+            node4._bias = -0.44;
+            node5._bias = 0.47;
+            node6._bias = 0.46;
+            node7._bias = 0.04;
+            node8._bias = -0.17;
+
+            // Connections creation
+            const connection1: ConnectionGene = new ConnectionGene(1, node1, node6, 0.41);
+            const connection2: ConnectionGene = new ConnectionGene(2, node1, node4, -0.15);
+            const connection3: ConnectionGene = new ConnectionGene(3, node1, node5, 0.51);
+            const connection4: ConnectionGene = new ConnectionGene(4, node2, node5, 0.32);
+            const connection5: ConnectionGene = new ConnectionGene(5, node2, node7, -0.69);
+            const connection6: ConnectionGene = new ConnectionGene(6, node2, node8, -0.42);
+            const connection7: ConnectionGene = new ConnectionGene(7, node7, node5, 0.17);
+            const connection8: ConnectionGene = new ConnectionGene(8, node8, node7, 0.67);
+            const connection9: ConnectionGene = new ConnectionGene(9, node8, node4, -0.44);
+            const connection10: ConnectionGene = new ConnectionGene(10, node7, node3, 0.22);
+            const connection11: ConnectionGene = new ConnectionGene(11, node6, node3, 0.50);
+
+
+            // Include nodes
+            myGenome.includeNode(node1);
+            myGenome.includeNode(node2);
+            myGenome.includeNode(node3);
+            myGenome.includeNode(node4);
+            myGenome.includeNode(node5);
+            myGenome.includeNode(node6);
+            myGenome.includeNode(node7);
+            myGenome.includeNode(node8);
+
+            // Include connections
+            myGenome.includeConnection(connection1);
+            myGenome.includeConnection(connection2);
+            myGenome.includeConnection(connection3);
+            myGenome.includeConnection(connection4);
+            myGenome.includeConnection(connection5);
+            myGenome.includeConnection(connection6);
+            myGenome.includeConnection(connection7);
+            myGenome.includeConnection(connection8);
+            myGenome.includeConnection(connection9);
+            myGenome.includeConnection(connection10);
+            myGenome.includeConnection(connection11);
+
+
+            // Set some variables
+            Reflect.set(myGenome, "inputNodeKeys", [1, 2]);
+            Reflect.set(myGenome, "outputNodeKeys", [3, 4, 5]);
+
+            // Run the function
+            const network: FeedForwardNetwork = FeedForwardNetwork.create(myGenome);
+            const output: number[] = network.activate([0.19, 0.78]);
+
+            // Tests
+            expect(output.length).toBe(3);
+            expect(output[0]).toBeCloseTo(0.68, 2);
+            expect(output[1]).toBeCloseTo(0.35, 2);
+            expect(output[2]).toBeCloseTo(0.71, 2);
         })
 })
